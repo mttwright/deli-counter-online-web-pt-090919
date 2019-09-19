46e9ca0b8b9b6f869c7e2a7e1c1fdb859a5c3b83
virtual-frame-7789 <mttwright@gmail.com>
2019-09-19 20:36:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,6 +9,6 @@ end
 def take_a_number(array, string)
   array.push(string)
   position = array.index(string) + 1
-  puts "welcome, #{string}. You are number #{position} in line."
+  "welcome, #{string}. You are number #{position} in line."
   array
 end
